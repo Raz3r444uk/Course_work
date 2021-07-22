81e2e692a725509448de487096e90d9b8d6c6fb9
Владимир Гуськов <raz3r444uk@mail.ru>
2021-07-22 17:09:59 +0400

    secont commit

---
+++ js/base.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  allowTouchMove: false,
+  slidesPerView: 1,
+  loop: true,
+  effect: 'fade',
+  speed: 5000,
+  autoplay: {
+    delay: 5000
+  }
+});
